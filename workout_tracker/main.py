e9dc29e492bc1aba9d914ca4d721fb50871a9390
minch <mincho.tanev@fdiba.tu-sofia.bg>
2023-04-28 01:56:34 +0300

    Add project

---
+++ workout_tracker/main.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+import requests
+import os
+
+APP_ID = os.environ.get("APP_ID")
+API_KEY = os.environ.get("API_KEY")
+BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+EXERCISE_ENDPOINTS = 'https://trackapi.nutritionix.com/v2/natural/exercise'
+
+post_params = {
+    'query': input("What exercises did u do today?"),
+    "gender": "male",
+    "weight_kg": 110.5,
+    "height_cm": 193,
+    "age": 20,
+}
+
+response = requests.post(url=EXERCISE_ENDPOINTS, json=post_params, headers=headers)
+result = response.json()['exercises'][0]
+
+POST_SHEETY = os.environ.get("POST_SHEETY")
+
+bearer_headers = {
+    "Authorization": "Bearer {}".format(BEARER_TOKEN)
+}
+
+add_workout = {
+    "workout": {
+        'date': "{}".format(datetime.now().strftime("%Y-%m-%d")),
+        "time": "{}".format(datetime.now().strftime("%H:%M:%S")),
+        "exercise": result["name"],
+        "duration": result["duration_min"],
+        "calories": result["nf_calories"],
+    }
+}
+
+post_workout = requests.post(url=POST_SHEETY, json=add_workout, headers=bearer_headers)
+print(post_workout.text)
+
+# del_workout = requests.delete(url=POST_SHEETY + "/{}".format(3), headers=bearer_headers) #Row serves as ObjectID
+# print(del_workout.text)
